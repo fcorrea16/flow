de1953bff3afbba0467da578872f741fa39da04b
Fernanda Correa <nandasc@gmail.com>
2015-07-19 14:19:40 -0400

    env pass changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url' : 'mongodb://ENV['dbuser']:ENV['dbpass']@ds033569.mongolab.com:33569/charts'
+    'url' : 'mongodb://process.env.dbuser:process.env.dbpass@ds033569.mongolab.com:33569/charts'
 
 };
 
