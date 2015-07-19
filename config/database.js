99c57c54a7410ada76d11ec44cc66bee3301b588
Fernanda Correa <nandasc@gmail.com>
2015-07-19 14:04:21 -0400

    db test

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url' : 'mongodb://MONGOLAB_USERNAME:MONGOLAB_PASS@ds033569.mongolab.com:33569/charts'
+    'url' : 'mongodb://nandasc:nanda16@ds033569.mongolab.com:33569/charts'
 
 };
 
