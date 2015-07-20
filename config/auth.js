a5a4198f2e9e68151c3cddcc60f8a5f24b0a936b
Fernanda Correa <nandasc@gmail.com>
2015-07-20 04:21:02 -0400

    env changes

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID'      : '656874289272-0ehu2a3dsk4b1k1ih27akqic8qvka20s.apps.googleusercontent.com',
-        'clientSecret'  : '_OMTkJcwo0REu7kfvGZzoWG2',
+        'clientID'      : process.env.googleid,
+        'clientSecret'  : process.env.googlesecret,
         'callbackURL'   : 'http://127.0.0.1:3000/auth/google/callback'
     }
 
