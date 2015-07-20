26d9baaaac230cedb6e76cfc6de72f57b1e4d957
Fernanda Correa <nandasc@gmail.com>
2015-07-20 09:47:31 -0400

    addinh home image

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,10 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-    // 'googleAuth' : {
-    //     'clientID'      : process.env.googleid,
-    //     'clientSecret'  : process.env.googlesecret,
-    //     'callbackURL'   : 'https://flowchartbuilder.herokuapp.com/auth/google/callback'
+    'googleAuth' : {
+        'clientID'      : process.env.googleid,
+        'clientSecret'  : process.env.googlesecret,
+        'callbackURL'   : 'https://flowchartbuilder.herokuapp.com/auth/google/callback'
     }
 
 };
